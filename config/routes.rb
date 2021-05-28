bde2001c59544c7bfaf0307e1936a16b66b68841
ibrahimmc <imoufarr@chicagobooth.edu>
2021-05-28 02:39:24 +0000

    Fixed issue with sign out

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,16 @@
 
 Rails.application.routes.draw do
   
+  
   root "conversations#index"
   resources :conversations
   resources :comments
   devise_for :users
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 
   resources :users, only: :show
 
-  #devise_scope :user do  
-  # get '/users/sign_out' => 'devise/sessions#destroy'     
-  #end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
