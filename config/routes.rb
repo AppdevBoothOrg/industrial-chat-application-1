4f845a214398dbb8f08f4c0b1754c6f6c86ecc03
ibrahimmc <imoufarr@chicagobooth.edu>
2021-05-28 22:10:17 +0000

    Routing for users/:username simplified, params now looks for a :username index rather than an :id

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
    get '/users/sign_out' => 'devise/sessions#destroy'     
   end
 
-  resources :users, only: :show
+  #resources :users, only: :show
+  get "/users/:username" => "users#show"
+  
   resources :ratings, only: :index
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
