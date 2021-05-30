09537cce9b015e92edc62c9ea5d0240daef6b4ae
ibrahimmc <imoufarr@chicagobooth.edu>
2021-05-30 04:46:21 +0000

    Started adding security: users cannot access other users' /user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  before_action :ensure_current_user_logged_in, only: %i[ show ]
+
   def show
-    @user = current_user
   end
+  
+  private
+
+    def set_user
+      if params[:username]
+        @user = User.find_by!(username: params.fetch(:username))
+      else
+        @user = current_user
+      end
+    end
+    
+    def ensure_current_user_logged_in
+      if current_user != @user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+      end
+    end
+
 end
